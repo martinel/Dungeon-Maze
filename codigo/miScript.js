8fbd40a9d062cb5d08797835cd66fce555eb9518
Paolo <paolo29_92@hotmail.com>
2011-11-22 09:24:40 -0500

    sin el hover de las imagenes

--- codigo/miScript.js
+++ codigo/miScript.js
@@ -1,21 +1,5 @@
 $('document').ready(function(){
-		$('#t1').hide();
-		$('#t2').hide();
-		$('#t3').hide();
-	$('#heroes img').hover(function(){
-		switch(this.id){
-			case 'guerrero':
-			$('#t1').show();
-			break;
-			case 'clerigo':
-			$('#t2').show();
-			break;
-			case 'valkiria':
-			$('#t3').show();
-			break;
-		}	
-	});
-	
+		
 	$('#pruebaimg').click(function(){
 	var valorimagen = $('input[name=select]:checked').val()
 
